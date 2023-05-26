1e21ba79dbcc43c5db91e560a0eb67aab658783b
HuangYanzhi <tangyanzhi@outlook.com>
2023-05-26 16:03:30 +0800

    build voc dataset for model

---
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_voc.py
@@ -0,0 +1,8 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    '../_base_/datasets/voc0712.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+data=dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1)
---
+++ configs/retinanet/retinanet_r101_fpn_1x_voc12.py
@@ -0,0 +1,38 @@
+_base_ = [
+    '../_base_/models/retinanet_r50_fpn.py',
+    '../_base_/datasets/voc0712.py',
+    '../_base_/schedules/schedule_1x.py', 
+    '../_base_/default_runtime.py']
+
+model = dict(
+    backbone=dict(
+        depth=101,
+        init_cfg=dict(type='Pretrained',
+                      checkpoint='torchvision://resnet101'),
+    bbox_head=dict(num_classes=20)
+    )
+)
+
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1,
+    train=dict(
+        dataset=dict(
+            ann_file=['data/VOCdevkit/VOC2012/ImageSets/Main/trainval.txt'],
+            img_prefix=['data/VOCdevkit/VOC2012/']
+        )
+    ),
+    val=dict(
+        ann_file='data/VOCdevkit/VOC2012/ImageSets/Main/test.txt',
+        img_prefix='data/VOCdevkit/VOC2012/'
+    ),
+    test=dict(
+        ann_file='data/VOCdevkit/VOC2012/ImageSets/Main/test.txt',
+        img_prefix='data/VOCdevkit/VOC2012/'
+    )
+)
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.0001, momentum=0.9, weight_decay=0.0001)
+
+auto_scale_lr = dict(enable=False, base_batch_size=1)
